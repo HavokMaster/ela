1b82d5cddacf06b666924b93f2387bc77276e281
HavokMaster <kevinsanthoshzundal@gmail.com>
2023-04-28 18:49:39 +0530

    Add existing project files to Git

---
+++ public/js/validate.js
@@ -0,0 +1,28 @@
+var password = document.getElementById("password");
+var confirm_password = document.getElementById("confirm-password");
+const togglePasswordButton = document.getElementById("toggle-password");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords do not match");
+        } 
+    else {
+        confirm_password.setCustomValidity("");
+        }
+}
+  
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+    
+togglePasswordButton.addEventListener("click", function() {
+    if (password.type === "password") {
+        password.type = "text";
+        confirm_password.type = "text";
+        togglePasswordButton.textContent = "Hide password";
+    } 
+    else {
+        password.type = "password";
+        confirm_password.type = "password";
+        togglePasswordButton.textContent = "Show password";
+      }
+});
