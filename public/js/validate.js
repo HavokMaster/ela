ff2d0debd3426d54042eec615c8e1a6e1ddeb653
Utsav Gandhi <Utsavgandhi3938@gmail.com>
2023-06-01 01:00:19 +0530

    UI changes
    login and signup page UI changes

--- public/js/validate.js
+++ public/js/validate.js
@@ -1,28 +1,37 @@
-var password = document.getElementById("password");
-var confirm_password = document.getElementById("confirm-password");
-const togglePasswordButton = document.getElementById("toggle-password");
+// var password = document.getElementById("password");
+// var confirm_password = document.getElementById("confirm-password");
+// const togglePasswordButton = document.getElementById("toggle-password");
 
-function validatePassword() {
-    if (password.value != confirm_password.value) {
-        confirm_password.setCustomValidity("Passwords do not match");
-        } 
-    else {
-        confirm_password.setCustomValidity("");
-        }
-}
+// function validatePassword() {
+//     if (password.value != confirm_password.value) {
+//         confirm_password.setCustomValidity("Passwords do not match");
+//         } 
+//     else {
+//         confirm_password.setCustomValidity("");
+//         }
+// }
   
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+// password.onchange = validatePassword;
+// confirm_password.onkeyup = validatePassword;
     
-togglePasswordButton.addEventListener("click", function() {
-    if (password.type === "password") {
-        password.type = "text";
-        confirm_password.type = "text";
-        togglePasswordButton.textContent = "Hide password";
-    } 
-    else {
-        password.type = "password";
-        confirm_password.type = "password";
-        togglePasswordButton.textContent = "Show password";
-      }
-});
+// togglePasswordButton.addEventListener("click", function() {
+//     if (password.type === "password") {
+//         password.type = "text";
+//         confirm_password.type = "text";
+//         togglePasswordButton.textContent = "Hide password";
+//     } 
+//     else {
+//         password.type = "password";
+//         confirm_password.type = "password";
+//         togglePasswordButton.textContent = "Show password";
+//       }
+// });
+
+const passwordInput = document.querySelector("#password")
+const eye = document.querySelector("#eye")
+
+eye.addEventListener("click", function(){
+    this.classList.toggle("fa-eye-slash")
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+    passwordInput.setAttribute("type", type)
+  })
