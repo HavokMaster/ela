a23e26730813dd349913f1e73998179f7387f429
HavokMaster <53501215+HavokMaster@users.noreply.github.com>
2023-06-05 17:20:55 +0530

    Updated install.bat
    -Install.bat now creates the ela database and initializes an admin account
    -Default admin id: 123456 pass: 1234

--- install.bat
+++ install.bat
@@ -5,4 +5,24 @@ mkdir question-paper
 mkdir question-bank
 mkdir mocktest
 cd ..
-npm install
+call npm install
+@echo off
+
+REM Define the MongoDB connection details
+set mongo_host=localhost
+set mongo_port=27017
+set database_name=ela
+
+REM Create the JavaScript code file
+echo const document = { name: 'admin', userId: '123456', password: '$2b$10$YTKS08yEEB8TCaMdnlDrxukhc8CdeFTkSvLF00DU6Lq2Vlv0wfENm', branch: 'cse', userType: 'admin' }; > insert.js
+echo use %database_name%; >> insert.js
+echo db.users.insertOne(document); >> insert.js
+
+REM Execute the MongoDB script using mongosh
+mongosh --host %mongo_host%:%mongo_port% --shell < insert.js
+
+REM Delete the JavaScript code file
+del insert.js
+
+REM Pause the script to view the output (optional)
+pause
