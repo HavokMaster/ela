f733a9052db8a378be1d576a7ccb6d2baa7f1cbb
HavokMaster <53501215+HavokMaster@users.noreply.github.com>
2023-06-05 17:38:09 +0530

    Updated install.bat
    -Fixed a bug where same user entry is done multiple times

--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ mkdir question-bank
 mkdir mocktest
 cd ..
 call npm install
+
 @echo off
 
 REM Define the MongoDB connection details
@@ -15,11 +16,19 @@ set database_name=ela
 
 REM Create the JavaScript code file
 echo const document = { name: 'admin', userId: '123456', password: '$2b$10$YTKS08yEEB8TCaMdnlDrxukhc8CdeFTkSvLF00DU6Lq2Vlv0wfENm', branch: 'cse', userType: 'admin' }; > insert.js
-echo use %database_name%; >> insert.js
-echo db.users.insertOne(document); >> insert.js
+echo use('%database_name%'); >> insert.js
+echo const collection = db.getCollection('users'); >> insert.js
+echo collection.createIndex({ userId: 1 }, { unique: true }); >> insert.js
+echo const existingUser = collection.findOne({ userId: document.userId }); >> insert.js
+echo if (!existingUser) { >> insert.js
+echo   collection.insertOne(document); >> insert.js
+echo   print('Document inserted successfully'); >> insert.js
+echo } else { >> insert.js
+echo   print('Document already exists'); >> insert.js
+echo } >> insert.js
 
 REM Execute the MongoDB script using mongosh
-mongosh --host %mongo_host%:%mongo_port% --shell < insert.js
+mongosh --host %mongo_host%:%mongo_port% --shell insert.js
 
 REM Delete the JavaScript code file
 del insert.js
